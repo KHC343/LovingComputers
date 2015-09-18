7cc5845b6b8c90b4c9bfe2d1ec0e45712622ea47
SRMorris <morrisherd@gmail.com>
2015-09-18 09:49:11 -0500

    First commit

---
+++ ComputerLoving/PongViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PongViewController.swift
+//  ComputerLoving
+//
+//  Created by smorris on 9/18/15.
+//  Copyright © 2015 LateNightGames. All rights reserved.
+//
+
+import UIKit
+
+class PongViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
