84eb37fc52ec58e6ac00e2bdc579780c35d9eeab
Stephan Carter <carter.stephan@comcast.net>
2015-09-18 09:30:26 -0500

    hope

--- ComputerLoving/DialougeViewController.swift
+++ ComputerLoving/DialougeViewController.swift
@@ -10,26 +10,77 @@ import UIKit
 
 class DialougeViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextView!
+    @IBOutlet weak var NextButton: UIButton!
+    @IBOutlet weak var Choice1: UIButton!
+    @IBOutlet weak var Choice2: UIButton!
+    @IBOutlet weak var Choice3: UIButton!
+    @IBOutlet weak var Choice4: UIButton!
+    var Mc = MCViewController()
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        Choice1.setTitle("", forState: UIControlState.Normal)
+        Choice2.setTitle("", forState: UIControlState.Normal)
+        Choice3.setTitle("", forState: UIControlState.Normal)
+        Choice4.setTitle("", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func Next(sender: AnyObject) {
+        count++
+        if(count == 1)
+        {
+            TextField.text = "Ha that's something I’ve never had."
+        }
+        if (count == 2)
+        {
+            TextField.text = "Confidence is for people who talk to others, who go outside. In other words what I’ve never been able to do.  It’s not that I haven't wanted to be active it’s just hard."
+        }
+        if (count == 3)
+        {
+            TextField.text = "I don’t remember when I started to be afraid of crowds maybe it has to do with my family. My Mother passed away when I was young leaving just me and my Dad. To keep us afloat my dad worked day and night to provide which left me alone in the house for the majority of the day."
+
+        }
+        if (count == 4)
+        {
+            TextField.text = "I became self dependent and spent most of my day alone on my computer or playing video games. I couldn’t even change myself if I wanted to and I do. Every time I have tried to be social I just fall back into old patterns, and now I’m going to college. Another four years of not fitting in. "
+        }
+        if (count == 5)
+        {
+            TextField.text = "thats where I found myself sitting in front of my computer just my blue desktop staring at me."
+        }
+        if (count == 6)
+        {
+            TextField.text = "How could I go to college it's nothing but being social. Maybe I can just withdraw from the school get a job at the supermarket or something."
+        }
+        if (count == 7)
+        {
+            TextField.text = "No my Dad worked hard for me how could I do that to him. Plus I need to leave for school in an hour."
+        }
+        if (count == 8)
+        {
+            NextButton.setTitle("", forState: UIControlState.Normal)
+            Choice1.setTitle("Get angry", forState: UIControlState.Normal)
+            Choice2.setTitle("Pout", forState: UIControlState.Normal)
+            Choice3.setTitle("Give up", forState: UIControlState.Normal)
+            Choice4.setTitle("Look on the bright side", forState: UIControlState.Normal)
+        }
+    }
+    @IBAction func Choice1(sender: AnyObject) {
+        Mc.one = 1
+    }
+    @IBAction func Choice2(sender: AnyObject) {
+        Mc.two = 1
+    }
+    @IBAction func Choice3(sender: AnyObject) {
+        Mc.two = 2
+    }
+    @IBAction func Choice4(sender: AnyObject) {
+        Mc.two = 4
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
--- ComputerLoving/MCViewController.swift
+++ ComputerLoving/MCViewController.swift
@@ -10,26 +10,20 @@ import UIKit
 
 class MCViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var one = 0
+    var two = 0
+    var three = 0
+    var four = 0
+    var gender = 0
+    var name = ""
+    
+    convenience init(MCname: String, Gender: Int)
+    {
+        self.init()
+        self.gender = 0
+        self.name = MCname
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
